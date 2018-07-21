4c41ca128ead27da56f0eb6813a4b57cbd9cfb07
Shlomi <shlomi@onca-inc.com>
2018-07-21 12:29:09 -0700

    Added additional routes for HTML pages and a basic test for each one of them.

--- test/test-server.js
+++ test/test-server.js
@@ -28,4 +28,49 @@ describe("Project Hub", function () {
                 expect(res).to.have.status(200);
             });
     });
+
+    it("Should get 200 code on project-create", function () {
+        return chai
+            .request(app)
+            .get("/project-create")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
+
+    it("Should get 200 code on project-delete", function () {
+        return chai
+            .request(app)
+            .get("/project-delete")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
+
+    it("Should get 200 code on project-edit", function () {
+        return chai
+            .request(app)
+            .get("/project-edit")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
+
+    it("Should get 200 code on project-view", function () {
+        return chai
+            .request(app)
+            .get("/project-view")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
+
+    it("Should get 200 code on projects-list", function () {
+        return chai
+            .request(app)
+            .get("/projects-list")
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
 });
